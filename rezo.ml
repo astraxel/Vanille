371da2d010f0686a2f2e3493b78430d3d0f4b6e6
Axel Kugelmann <axel.kugelmann@ens.fr>
2018-04-28 13:54:20 +0200

    Implémentation de la routine serveur

--- rezo.ml
+++ rezo.ml
@@ -11,7 +11,7 @@ module type S = sig
   val doco: unit process list -> unit process
 
   val return: ?flags: flag list -> 'a -> 'a process
-  val bind: ?flags: flag list -> 'a process -> ('a -> 'b process) -> 'b process
+  val bind: 'a process -> ('a -> 'b process) -> 'b process
 
   val run: 'a process -> 'a
 end
@@ -57,7 +57,7 @@ module Th: S = struct
             
   type 'a process = { proc : (unit -> 'a);
                       flags : flag list;
-                      id : int;
+                      mutable id : int;
                     }
                     (* Chaque process aura un unique moyen pour communiquer avec le processus
                        principal de la machine qui s'occupe de la communication avec le serveur.
@@ -87,16 +87,42 @@ module Th: S = struct
                    data : 'a;
                  }
 
+  type doco_request = { content : unit process list;
+                        target : int; (*id du processus hebergeant le doco*)
+                      }
+
   type 'a communication = DC_QUERY
-                        | DC_CONFIRM (*Quand le client n'a plus de processus actifs*)
+                        | DC_CONFIRM (*Message sent by servor to disconnect client*)
                         | CHANNEL_QUERY
                         | CHANNEL of int
                         | DATA of 'a data
                         | DATA_REQUEST of 'a port
-                        | DOCO of unit process list
-                        | DOCO_CONFIRM of int (*Message du serveur à un processus signalant que les processus parallèles sont terminés*)
+                        | DOCO of doco_request
+                        | DOCO_CONFIRM of int (*Message du serveur à un processus signalant que
+                                                les processus parallèles sont terminés*)
                         | EXECUTE of 'a process
-                        | RESULT of 'a data (*ici, le target est le numéro du processus envoyant le résultat*)
+                        | RESULT of 'a data (*ici, le target est le numéro du processus
+                                              envoyant le résultat*)
+
+
+  (* TYPES SERVEUR *)
+  type doco_count = { mutable current : int;
+                      total : int;
+                      client : int;
+                    }
+
+  type client_state = { mutable load : int;
+                        mutable connected : bool; (*connected = false => le client a initialisé
+                                                    la procédure de déconnexion*)
+                      }
+
+  type pipe = { input : Unix.file_descr;
+                output : Unix.file_descr
+              }
+
+  type pipe_request = { pipe_id : int;
+                        client_id : int
+                      }
                            
                                 
 
@@ -110,7 +136,8 @@ module Th: S = struct
   let output = ref None (*Some file_descr*)
 
   (*Client
-    Note : les processus sont identifiés pour chaque machine par un id. Au plus max_int processus peuvent être exécutés sur une même connexion.
+    Note : les processus sont identifiés pour chaque machine par un id.
+    Au plus max_int processus peuvent être exécutés sur une même connexion.
     Les processus bindés ne comptent pas pour de nouveau processus *)
   let proc_inputs = Hashtbl.create 5 (*file_descr des inputs des process*)
   let proc_outputs = Hashtbl.create 5 (*file_descr des outputs des process*)
@@ -121,36 +148,26 @@ module Th: S = struct
 
   (*Serveur*)
                 
-  let new_channel = let r=ref 0 in (fun () -> incr r; !r)
+  let new_chan = let r=ref 0 in (fun () -> incr r; !r)
   let new_proc = let s=ref 0 in (fun () -> incr s; !s)
+  let new_client = let t=ref 0 in (fun () -> incr t; !t)
                               
   let client_inputs = Hashtbl.create 5
   let client_outputs = Hashtbl.create 5
-  let client_charge = Hashtbl.create 5 (*Nb de processus en cours sur le client*)
-  let valid_clients = ref [] (*Liste des clients qui ne se sont pas déconnectés*)
-             
+  let clients = Hashtbl.create 5 (*Ensemble des clients de type client_state*)
+  let doco_counts = Hashtbl.create 5 (*contient des doco_count pour chaque process de type doco*)
+  let pipes = Hashtbl.create 5 (*contient les file_descr des pipes*)
+  let waiting_for_data = ref [] (*Liste des channels en attente de données.
+                                  On pourrait faire un fork et un read bloquant, mais après
+                                  il y aurai peut-être des choses bizarres lors du write dans le
+                                  socket pour l'envoi*)
+  let global_result = ref None (*résultat final de l'exécution
+                                 C'est le résultat du processus d'id 0
+                                 Si cette variable ne contient pas None, la routine serveur
+                                 termine.*)
 
-
-  (* FONCTIONS CLIENT *)               
-             
-  (*Fonctions de lecture et d'écriture*)
-                 
-  let send_data comm =
-    match (!output) with
-    |Some chan ->
-      let b = Marshal.to_bytes comm [Closures] in
-      if Bytes.length b > max_comm_size then failwith "trying to send data too big";
-      let _ = Unix.write chan b 0 (Bytes.length b) in
-      ()
-    |None -> failwith "No communications are enabled for this process"
-
-  let send_bytes b length=
-    match (!output) with
-    |Some chan ->
-      let _ = Unix.write chan b 0 length in
-      ()
-    |None -> failwith "No communications are enabled for this process"
-
+  (* FONCTIONS DE LECTURE ET D'ECRITURE *)
+                  
   let read_comm fd =
     let n = Unix.read fd buffer 0 Marshal.header_size in
     if n=0 then
@@ -163,6 +180,30 @@ module Th: S = struct
       else
         size + Marshal.header_size
 
+  let write_comm comm fd =
+    let b = Marshal.to_bytes comm [Closures] in
+    if Bytes.length b > max_comm_size then failwith "trying to send data too big";
+    let _ = Unix.write fd b 0 (Bytes.length b) in
+    ()
+
+
+  (* FONCTIONS CLIENT *)               
+             
+  (*Fonctions de lecture et d'écriture*)
+                 
+  let send_data comm =
+    match (!output) with
+    |Some chan -> write_comm comm chan
+    |None -> failwith "No communications are enabled for this process"
+
+  let send_bytes b length=
+    match (!output) with
+    |Some chan ->
+      let _ = Unix.write chan b 0 length in
+      ()
+    |None -> failwith "No communications are enabled for this process"
+
+
   let get_data () =
     match (!input) with
     |Some chan ->
@@ -170,7 +211,7 @@ module Th: S = struct
         |0 -> loop ()
         |_ -> ()
       in loop ();
-      let (d: 'a communication) = Marshal.from_bytes buffer 0 in d (*On assure le typage de d*)                 
+      let (d: 'a communication) = Marshal.from_bytes buffer 0 in d (*On assure le typage de d*)
     |None -> failwith "No communications are enabled for this process"
 
 
@@ -179,16 +220,18 @@ module Th: S = struct
   let transmit_proc_comm proc_id proc_output =
     match read_comm proc_output with
     |0 -> ()
-    |n -> send_bytes buffer n; (*Vérifier que write ne modifie pas le buffer*)
-          let (d: 'a communication) = Marshal.from_bytes buffer 0 in
-          match d with
-          |DATA d -> ()
-          |RESULT d -> Hashtbl.remove proc_inputs proc_id;
-                       Hashtbl.remove proc_outputs proc_id 
-          |DATA_REQUEST port -> Hashtbl.add pipe_process_corres port proc_id
-          |CHANNEL_QUERY -> Queue.push proc_id need_pipe
-          |DOCO l -> ()
-          |_ -> failwith "a process cannot generate such communication"
+    |n -> let (d: 'a communication) = Marshal.from_bytes buffer 0 in
+          begin
+            match d with
+            |DATA d -> ()
+            |RESULT _ -> Hashtbl.remove proc_inputs proc_id;
+                         Hashtbl.remove proc_outputs proc_id 
+            |DATA_REQUEST port -> Hashtbl.add pipe_process_corres port proc_id
+            |CHANNEL_QUERY -> Queue.push proc_id need_pipe
+            |DOCO _ -> ()
+            |_ -> failwith "a process cannot generate such communication"
+          end;
+          send_bytes buffer n (*Vérifier que write ne modifie pas le buffer*)
 
 
   (*Gestion de l'interface client/serveur*)
@@ -233,6 +276,7 @@ module Th: S = struct
       end
     |DOCO_CONFIRM proc ->
       write_in_proc size proc
+    |DC_CONFIRM -> exit 0
     |_ -> failwith "Une instruction n'ayant aucun sens a été reçue du serveur"
 
   let transmit_server_comm () =
@@ -250,10 +294,10 @@ module Th: S = struct
 
   (*Fonctions de connexion*)
 
-  let rec routine socket =
+  let rec client_routine socket =
     Hashtbl.iter transmit_proc_comm proc_outputs;
     transmit_server_comm ();
-    routine socket
+    client_routine socket
     
 
   let make_addr name port =
@@ -266,31 +310,22 @@ module Th: S = struct
     f socket
 
   let init serv port =
-    run_client routine (make_addr serv port)
-    
+    run_client client_routine (make_addr serv port)
 
-
-
-  (* FONCTIONS SERVEUR *)
-
-  let rec accept_clients () =
-    assert false (*C'est le plus compliqué*)
-
-
-
-    
-
-
-    
 
   (*FONCTIONS D'ÉCRITURE DE PROGRAMMES*)
     
 
   let new_channel () =
-    assert false (*TODO : Demander au serveur un id de pipe frais*)
+    send_data (CHANNEL_QUERY);
+    let d = get_data () in
+    match d with
+    |CHANNEL c -> c,c
+    |_ -> failwith "Wrong communication input, expected a channel"
 
     
-  (* Attention : buffer des pipes de 65536 octets, il faut donc fork avant d'écrire -> non, tout sera stocké sur le serveur *)
+  (* Attention : buffer des pipes de 65536 octets, il faut donc fork avant d'écrire 
+     -> non, tout sera stocké sur le serveur *)
 
   let put ?flags:(flags = []) x c =
     let p = fun () -> send_data (DATA { target = c; data = x })
@@ -305,7 +340,7 @@ module Th: S = struct
                     failwith "Wrong target for communication"
                   else
                     x.data
-                |_ -> failwith "Wrong communication input"
+                |_ -> failwith "Wrong communication input, expected some data"
     in {proc = p; flags = flags; id = -1}
 
 
@@ -313,28 +348,169 @@ module Th: S = struct
     let p= fun () -> x
     in {proc = p; flags = flags; id = -1}
 
-  let bind ?flags:(flags = []) p1 f =
+  let bind p1 f =
     let p = fun () -> let v = p1.proc () in
                       let p2 = f v in
                       p2.proc ()
-    in {proc = p; flags = flags; id = p1.id}
+    in {proc = p; flags = p1.flags; id = p1.id}
 
-  let distribute l =
+  let send_doco l =
     assert false (*TODO : envoyer au serveur la liste de processus à répartir*)
 
 
   let doco l =
-    {proc = (fun () -> distribute l); flags = []; id = -1}
+    {proc = (fun () -> send_doco l); flags = []; id = -1}
     
 
 
 
   (* FONCTIONS SERVEUR *)
 
-  (*TODO : faire la fonction serveur qui écoute les requetes des clients,
-    et celle qui organise la répartition des ressources*)
+  let rec accept_clients () =
+    assert false (*C'est le plus compliqué*)
+
+  let send_message client_id comm =
+    match Hashtbl.find_opt client_inputs client_id with
+    |None -> failwith "Envoi d'une donnée à un client inexistant"
+    |Some fd -> write_comm comm fd
+
+  let init_disconnection client_id =
+    let state = Hashtbl.find clients client_id in
+    state.connected <- false
+
+
+  let find_minimal_client () =
+    let f client state res =
+      let (minload,minclient) = res in
+      if state.connected && (minclient = -1 || state.load < minload) then
+        (state.load, client)
+      else
+        res
+    in snd (Hashtbl.fold f clients (-1,-1))
+
+  let incr_load client_id = match Hashtbl.find_opt clients client_id with
+    |None -> failwith "Tentative d'attribution de processus à un client inexistant"
+    |Some state -> state.load <- state.load + 1
+
+  let rec distribute (l: unit process list) target = match l with
+    |[] -> ()
+    |p::q -> let id = new_proc () in
+             p.id <- id;
+             let final_process = bind p (fun () -> return target) in
+             let c = find_minimal_client () in
+             if c= -1 then failwith "No client available for this process";
+             incr_load c;
+             send_message c (EXECUTE final_process)
+             
+             
+
+
+  let handle_communication client_id size d = match d with
+    |DC_QUERY -> init_disconnection client_id
+    |CHANNEL_QUERY ->
+      let c = new_chan () in
+      send_message client_id (CHANNEL c)
+    |DATA d ->
+      begin
+        match (Hashtbl.find_opt pipes d.target) with
+        |Some p -> let _ = Unix.write p.input buffer 0 size in ()
+        |None -> let temp_output, temp_input = Unix.pipe () in
+                 Hashtbl.add pipes d.target { input = temp_input ; output = temp_output};
+                 let _ = Unix.write temp_input buffer 0 size in ()
+      end
+    |DATA_REQUEST channel ->
+      waiting_for_data := {pipe_id = channel; client_id = client_id}::(!waiting_for_data)
+    |DOCO x -> let length = List.length x.content in
+               Hashtbl.add doco_counts x.target {current = 0; total = length; client = client_id};
+               distribute x.content x.target
+    |RESULT x ->
+      begin
+        try
+          let state = Hashtbl.find clients client_id in
+          state.load <- state.load -1
+        with Not_found -> failwith "Retour d'un client inexistant"
+      end;
+      if x.target = 0 then
+        global_result := Some x.data
+      else
+        let parent = x.data in
+        begin
+          try
+            let count = Hashtbl.find doco_counts parent in
+            count.current <- count.current + 1;
+            if count.current = count.total then
+              begin
+                send_message count.client (DOCO_CONFIRM parent);
+                Hashtbl.remove doco_counts parent
+              end
+          with Not_found -> failwith "Présence d'un processus orphelin"
+        end
+    |_ -> failwith "Communication impossible vers le serveur reçue"
+    
+
+  let rec handle_client client_id client_output =
+    match read_comm client_output with
+    |0 -> ()
+    |n -> let (d: 'a communication) = Marshal.from_bytes buffer 0 in
+          handle_communication client_id n d;
+          handle_client client_id client_output
+
+  let handle_data_requests () =
+    let rec aux l = match l with
+      |[] -> []
+      |x::q ->
+        match (Hashtbl.find_opt pipes x.pipe_id) with
+        |None -> x::(aux q)
+        |Some p ->
+          let n = Unix.read p.output buffer 0 Marshal.header_size in
+          if n = 0 then
+            x::(aux q)
+          else
+            let size = Marshal.data_size buffer 0 in
+            let _ = Unix.read p.output buffer Marshal.header_size size in
+            try
+              let fd = Hashtbl.find client_inputs x.client_id in
+              let _ = Unix.write fd buffer 0 (size + Marshal.header_size) in
+              aux q
+            with
+              Not_found -> failwith "Tentative de communication avec un client inexistant"
+             |_ -> failwith "Une erreur est survenue lors de l'envoi des données"
+    in      
+    waiting_for_data := aux (!waiting_for_data)
+
+  let disconnect_clients () =
+    let f client_id state l=
+      if (not state.connected) && (state.load = 0) then
+        client_id::l
+      else
+        l
+    in
+    let l_dc = Hashtbl.fold f clients [] in
+    let g client_id =  Hashtbl.remove client_inputs client_id;
+                       Hashtbl.remove client_outputs client_id;
+                       Hashtbl.remove clients client_id
+    in
+    List.iter g l_dc  
+          
+
+  let rec server_routine () =
+    disconnect_clients ();
+    accept_clients ();
+    Hashtbl.iter handle_client client_outputs;
+    handle_data_requests ();
+    match (!global_result) with
+    |None -> server_routine ()
+    |Some x -> x
+    
+
 
   let run p = assert false (* TODO : init le serveur, les relations avec les clients, 
-                              et lancer le process*)
+                              et lancer le process*)   
+
+
+    
+    
+
+
             
 end
